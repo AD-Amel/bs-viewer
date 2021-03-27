50d20c45bada88f88672ccdda3ac7fde3d989076
Artem Pashkov <alucardyanas@gmail.com>
2021-03-27 14:24:34 +0300

    component wargear partly added

---
+++ src/mock/wargear.ts
@@ -0,0 +1,51 @@
+export const wargear = [{
+    "id": "fceb-2360-07e4-2347",
+    "name": "Spiculus Bolt Launcher",
+    "entryId": "2b51-9184-6c6c-2260::4a5d-97a0-725b-3d8c::5ab8-62de-01b0-01bc",
+    "number": "1",
+    "type": "upgrade",
+    "profiles": {
+      "profile": {
+        "id": "2b51-9184-6c6c-2260::4a5d-97a0-725b-3d8c::7d49-2346-bc6f-501f",
+        "name": "Spiculus Bolt Launcher",
+        "hidden": "false",
+        "typeId": "d5f97c0b-9fc9-478d-aa34-a7c414d3ea48",
+        "typeName": "Weapon",
+        "characteristics": {
+          "characteristic": [
+            {
+              "name": "Range",
+              "typeId": "6fa97fa8-ea74-4a27-a0fb-bc4e5f367464",
+              "#text": "24\""
+            },
+            {
+              "name": "Type",
+              "typeId": "077c342f-d7b9-45c6-b8af-88e97cafd3a2",
+              "#text": "Heavy 5"
+            },
+            {
+              "name": "S",
+              "typeId": "59b1-319e-ec13-d466",
+              "#text": "5"
+            },
+            {
+              "name": "AP",
+              "typeId": "75aa-a838-b675-6484",
+              "#text": "-1"
+            },
+            {
+              "name": "D",
+              "typeId": "ae8a-3137-d65b-4ca7",
+              "#text": "1"
+            },
+            {
+              "name": "Abilities",
+              "typeId": "837d-5e63-aeb7-1410",
+              "#text": "Each time this weapon is selected to shoot with, if this model has not moved this turn, change this weapon's type to Heavy 10."
+            }
+          ]
+        }
+      }
+    },
+
+}]
