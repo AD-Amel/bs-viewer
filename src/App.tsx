ad06964f2b940321d385e72a2d0ed2d12dd435ca
Artem Pashkov <alucardyanas@gmail.com>
2021-03-28 19:57:51 +0300

    cpmponent wargear v.2

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.css';
 import Button from '@material-ui/core/Button';
 import { testAbilities } from './mock/abilities';
 import { Abilities } from './Components/Abilities/Abilities';
+import { Wargear } from './Components/Wargear/Wargear';
+import { wargear } from './mock/wargear';
 
 function App() {
     const unitAbilitiesData = testAbilities;
---
+++ src/models/wargear.ts
@@ -0,0 +1,9 @@
+export interface Wargear {
+    name: string;
+    characteristics: {
+        characteristic: {
+            '#name': string
+            '#text': string;
+        }
+    }
+}
